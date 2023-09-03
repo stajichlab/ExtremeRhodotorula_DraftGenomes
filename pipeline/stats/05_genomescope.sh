31a391e7f766531cc9c9685147852c2a670e5841
Jason Stajich <jasonstajich.phd@gmail.com>
2023-09-02 21:44:46 -0700

    search for non-empty

--- pipeline/stats/05_genomescope.sh
+++ pipeline/stats/05_genomescope.sh
@@ -36,13 +36,28 @@ IFS=, # set the delimiter to be ,
 tail -n +2 $SAMPLEFILE | sed -n ${N}p | while read ID BASE SRA SPECIES STRAIN LOCUSTAG BIOPROJECT BIOSAMPLE NOTES
 do
     # for this project two different file base but this needs fixing otherse)
-    if [ ! -f $GENOMESCOPE/$STRAIN.histo ]; then
-	if [ ! -f $FASTQFOLDER/${BASE}_1.fastq.gz ]; then
-		jellyfish count -C -m $KMER -s $JELLYFISHSIZE -t $CPU -o $SCRATCH/$STRAIN.jf <(pigz -dc $FASTQFOLDER/${BASE})
-	else
-    		jellyfish count -C -m $KMER -s $JELLYFISHSIZE -t $CPU -o $SCRATCH/$STRAIN.jf <(pigz -dc $FASTQFOLDER/${BASE}_[12].fastq.gz)
-	fi
-    	jellyfish histo -t $CPU $SCRATCH/$STRAIN.jf > $GENOMESCOPE/$STRAIN.histo
-    	Rscript scripts/genomescope.R $GENOMESCOPE/$STRAIN.histo $KMER $READLEN $GENOMESCOPE/$STRAIN/
-    fi
+  if [ ! -s $GENOMESCOPE/$STRAIN.histo ]; then
+    echo "running genomescope on $STRAIN"
+	  if [ ! -f $FASTQFOLDER/${BASE}_1.fastq.gz ]; then
+      COUNT=$(echo -n $BASE | perl -p -e 's/;/\n/g' | wc -l | awk '{print $1}')
+      if [[ $COUNT == 0 ]]; then
+        FNAME=$FASTQFOLDER/${BASE}
+        echo "FNAME is $FNAME"
+      else
+        for file in $(echo -n $BASE | perl -p -e 's/;/,/g')
+        do
+          cat $FASTQFOLDER/$file
+        done > $SCRATCH/${ID}_all.fastq.gz
+        FNAME=$SCRATCH/${ID}_all.fastq.gz
+      fi
+      jellyfish count -C -m $KMER -s $JELLYFISHSIZE -t $CPU -o $SCRATCH/$STRAIN.jf <(pigz -dc $FNAME)
+      rm -f $SCRATCH/${ID}_all.fastq.gz
+    else
+      jellyfish count -C -m $KMER -s $JELLYFISHSIZE -t $CPU -o $SCRATCH/$STRAIN.jf <(pigz -dc $FASTQFOLDER/${BASE}_[12].fastq.gz)
+	  fi
+    jellyfish histo -t $CPU $SCRATCH/$STRAIN.jf > $GENOMESCOPE/$STRAIN.histo
+    Rscript scripts/genomescope.R $GENOMESCOPE/$STRAIN.histo $KMER $READLEN $GENOMESCOPE/$STRAIN/
+  else
+    echo "skipping ... $STRAIN already processed"
+  fi
 done
