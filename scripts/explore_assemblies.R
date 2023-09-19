cb69087cf2141443939c34a9d3a416d514e13019
Jason Stajich <jasonstajich.phd@gmail.com>
2023-09-18 18:02:59 -0700

    replot

--- scripts/explore_assemblies.R
+++ scripts/explore_assemblies.R
@@ -21,18 +21,18 @@ p <- ggplot(data = merged, mapping = aes(x = Species, y = TOTAL_LENGTH/10**6,col
   scale_color_manual(values=getPalette) +
   ylab("Length (Mb)") 
 
-ggsave("genome_size_boxplot.pdf",p,width=18,height=14)
+ggsave("genome_size_boxplot.pdf",p,width=8,height=6)
 
 p2 <- ggplot(data = merged %>% filter(BUSCO_Complete > 75), 
              mapping = aes(x = log(Average_Coverage)/log(10), y=BUSCO_Complete,color=Species)) +
   geom_point()  +  theme_cowplot(12) + 
   scale_color_manual(values=getPalette) + xlab("Coverage (log(10))") + ylab("BUSCO Completeness") 
-ggsave("genome_coverage.pdf",p2,width=18,height=14)
+ggsave("genome_coverage.pdf",p2,width=9,height=10)
 
 p3 <- ggplot(data = merged %>% filter(BUSCO_Complete > 75), 
              mapping = aes(x = TOTAL_LENGTH/10**6, y=BUSCO_Duplicate,color=Species)) +
   geom_point()  +  theme_cowplot(12)  + 
   scale_color_manual(values=getPalette) + xlab("Genome size (Mb)") + ylab("BUSCO Duplication")
-ggsave("genome_dup_by_size.pdf",p3,width=18,height=14)
+ggsave("genome_dup_by_size.pdf",p3,width=9,height=9)
 
 
